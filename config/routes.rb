db56c508d84a0177d2ec4d02f1d7d39ea6e48f3c
no215tyler <no.215.tyler@gmail.com>
2023-12-24 06:33:23 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users #<= この中にuserモデル関連のルーティングが組まれている？からresources内に:destroyの記述をしなくてもログアウトが動作するのか？
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end

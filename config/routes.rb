5fde58641a89dafd050158c6f182427773fca5c7
no215tyler <no.215.tyler@gmail.com>
2023-12-23 11:06:32 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users #<= この中にuserモデル関連のルーティングが組まれている？からresources内に:destroyの記述をしなくてもログアウトが動作するのか？
   root to: "messages#index"
   resources :users, only: [:edit, :update]
 end

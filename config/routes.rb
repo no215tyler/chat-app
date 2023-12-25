74f0645b7ed0ee797e3eed011f2c8e7ae54f62d7
no215tyler <no.215.tyler@gmail.com>
2023-12-25 10:24:24 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users #<= この中にuserモデル関連のルーティングが組まれている？からresources内に:destroyの記述をしなくてもログアウトが動作するのか？
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
